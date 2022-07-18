ab794271074eb854bdd368a9b81917f12d6bfe6f
Mayank Khanna <Mayankkhanna015@icloud.com>
2022-07-18 19:07:59 +0530

    Footer Routing1

--- mobile-web-service-provider/tailwind.config.js
+++ mobile-web-service-provider/tailwind.config.js
@@ -1,6 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
+  important: true,
   purge: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./src/Components/**/*.{js,ts,jsx,tsx}",
