cbf966df55029001c517de7c08dc565a18b042cf
Mayank Khanna <Mayankkhanna015@icloud.com>
2022-08-05 17:31:14 +0530

    google ads

--- mobile-web-service-provider/pages/_document.js
+++ mobile-web-service-provider/pages/_document.js
@@ -23,6 +23,23 @@ class MyDocument extends Document {
                   `,
             }}
           />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=UA-236120560-1`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'UA-236120560-1', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+            }}
+          />
           {/* <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=G-FGC2M95D0F`}
