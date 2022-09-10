3fd601a847e2176ec29fd9f029a8afb8a7aa1bdc
Mayank Khanna <Mayankkhanna015@icloud.com>
2022-09-10 15:57:50 +0530

    Adding Hotjar

--- mobile-web-service-provider/pages/_document.js
+++ mobile-web-service-provider/pages/_document.js
@@ -1,11 +1,11 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 // import { GA_TRACKING_ID } from "../src/lib/gtag";
 class MyDocument extends Document {
   render() {
     return (
-      <Html className="scroll-smooth">
+      <Html className='scroll-smooth'>
         <Head>
-          <link rel="icon" href="/favicon.ico" />
+          <link rel='icon' href='/favicon.ico' />
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=G-775ZLZW0P7`}
@@ -35,6 +35,19 @@ class MyDocument extends Document {
                     gtag('js', new Date());
 
                     gtag('config', 'UA-236120560-1');
+                  `,
+            }}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:3151576,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
                   `,
             }}
           />
@@ -77,81 +90,81 @@ class MyDocument extends Document {
             gtag("js", new Date()); gtag("config", "G-FGC2M95D0F");
           </script> */}
           <meta
-            name="keywords"
+            name='keywords'
             content={
-              "cheap website," +
-              "website for business," +
-              "website for startups," +
-              "web services," +
-              "web development services," +
-              "cheap web application" +
-              "web development," +
-              "high quality website," +
-              "custom website," +
-              "custom website development," +
-              "website development," +
-              "new website," +
-              "website for me," +
-              "website for business," +
-              "web solutions," +
-              "cheap mobile apps," +
-              "mobile apps for business," +
-              "mobile apps for startups," +
-              "mobile app services," +
-              "mobile development services," +
-              "cheap mobile application" +
-              "mobile app development," +
-              "high quality mobile apps," +
-              "custom mobile apps," +
-              "custom mobile app development," +
-              "mobile apps development," +
-              "new mobile apps," +
-              "mobile apps for me," +
-              "mobile apps for business," +
-              "mobile app solutions," +
-              "best web building sites," +
-              "google new website," +
-              "make a new website," +
-              "build a new website," +
-              "i need a website," +
-              "i need a website for my business," +
-              "i need an ecommerce website," +
-              "i need a business website," +
-              "need a website," +
-              "build custom website," +
-              "personalized website," +
-              "custom web development services," +
-              "custom web design services," +
-              "customize website," +
-              "custom web design company," +
-              "custom website design company," +
-              "affordable custom websites," +
-              "custom ecommerce website," +
-              "custom website design cost," +
-              "affordable custom website design," +
-              "cheap website builders," +
-              "affordable web design," +
-              "cheap website design," +
-              "affordable website design," +
-              "cheap website makers," +
-              "cheap web design," +
-              "affordable website," +
-              "cheap website creator," +
-              "affordable web design services," +
-              "affordable web design company," +
-              "affordable website design near me," +
-              "cheap ecommerce website," +
-              "affordable websites for small business," +
-              "affordable website designer," +
-              "affordable custom websites," +
-              "affordable website builders," +
-              "best cheap website builders," +
-              "affordable website design packages," +
-              "a one website services," +
-              "a one website solutions," +
-              "website management," +
-              "new cheap website," +
-              "low cost website design," +
+              'cheap website,' +
+              'website for business,' +
+              'website for startups,' +
+              'web services,' +
+              'web development services,' +
+              'cheap web application' +
+              'web development,' +
+              'high quality website,' +
+              'custom website,' +
+              'custom website development,' +
+              'website development,' +
+              'new website,' +
+              'website for me,' +
+              'website for business,' +
+              'web solutions,' +
+              'cheap mobile apps,' +
+              'mobile apps for business,' +
+              'mobile apps for startups,' +
+              'mobile app services,' +
+              'mobile development services,' +
+              'cheap mobile application' +
+              'mobile app development,' +
+              'high quality mobile apps,' +
+              'custom mobile apps,' +
+              'custom mobile app development,' +
+              'mobile apps development,' +
+              'new mobile apps,' +
+              'mobile apps for me,' +
+              'mobile apps for business,' +
+              'mobile app solutions,' +
+              'best web building sites,' +
+              'google new website,' +
+              'make a new website,' +
+              'build a new website,' +
+              'i need a website,' +
+              'i need a website for my business,' +
+              'i need an ecommerce website,' +
+              'i need a business website,' +
+              'need a website,' +
+              'build custom website,' +
+              'personalized website,' +
+              'custom web development services,' +
+              'custom web design services,' +
+              'customize website,' +
+              'custom web design company,' +
+              'custom website design company,' +
+              'affordable custom websites,' +
+              'custom ecommerce website,' +
+              'custom website design cost,' +
+              'affordable custom website design,' +
+              'cheap website builders,' +
+              'affordable web design,' +
+              'cheap website design,' +
+              'affordable website design,' +
+              'cheap website makers,' +
+              'cheap web design,' +
+              'affordable website,' +
+              'cheap website creator,' +
+              'affordable web design services,' +
+              'affordable web design company,' +
+              'affordable website design near me,' +
+              'cheap ecommerce website,' +
+              'affordable websites for small business,' +
+              'affordable website designer,' +
+              'affordable custom websites,' +
+              'affordable website builders,' +
+              'best cheap website builders,' +
+              'affordable website design packages,' +
+              'a one website services,' +
+              'a one website solutions,' +
+              'website management,' +
+              'new cheap website,' +
+              'low cost website design,' +
               `A-ONE WEB SOLUTIONS is an ambitious and motivated service
             provider tailored to the needs of businesses of every esteemed
             composition. We are focused on providing services that are
@@ -159,10 +172,10 @@ class MyDocument extends Document {
             Serving many international clients, we have a vast experience in
             serving all types of clients in a very reasanable and quality
             focused mindset.,` +
-              "International Clients"
+              'International Clients'
             }
           />
-          <meta name="author" content={"Mayank Khanna"} />
+          <meta name='author' content={'Mayank Khanna'} />
         </Head>
         <body>
           <Main />
